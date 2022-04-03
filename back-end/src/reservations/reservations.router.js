dce1083172c5bb064a036f5668fa47896b2e8540
Cesar Sanchez <cesarsnchz13@gmail.com>
2022-04-02 22:56:54 -0500

    fixed typo in methods not allowed

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const controller = require("./reservations.controller");
-const methodNotAllowed = require("../errors/methodNotAllowed");
+const methodNotAllowed = require("../errors/MethodNotAllowed");
 
 router
   .route("/")
