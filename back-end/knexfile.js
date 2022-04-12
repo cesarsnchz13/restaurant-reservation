2f0f26f44e905172687a8e62418c556c41ff9da4
Cesar Sanchez <cesarsnchz13@gmail.com>
2022-04-12 14:23:16 -0500

    updated env urls

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -4,14 +4,14 @@
  * You will not need to make changes to this file.
  */
 
-require('dotenv').config();
+require("dotenv").config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://zfbobdvu:aixdSnAL2L5ziI9wOxKA_1bXzVgQMG2n@hansken.db.elephantsql.com/zfbobdvu",
+  DATABASE_URL_DEVELOPMENT = "postgres://fxfwxvry:FOna52Jw1w6s32wwBzES0-xg1l2cevjd@hansken.db.elephantsql.com/fxfwxvry",
+  DATABASE_URL_TEST = "postgres://yfiqlppc:MisQmtVzWXT4Z0kijdRPBPb3afyYfWLd@hansken.db.elephantsql.com/yfiqlppc",
+  DATABASE_URL_PREVIEW = "postgres://ideetfao:i4IYelVav2kE4nPk6Mne5Xxz35MxM3mG@hansken.db.elephantsql.com/ideetfao",
   DEBUG,
 } = process.env;
 
